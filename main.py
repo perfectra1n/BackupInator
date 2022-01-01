497bc8a621b563e6a324c68d869621f1115fd368
perf3ct <jonfuller2012@gmail.com>
2022-01-01 11:18:55 -0800

    update 1

--- main.py
+++ main.py
@@ -1,15 +1,57 @@
+#!/usr/bin/env python3
 import argparse
+import pyfiglet
+import pysftp
 
 # Local import
 import log
+
+def get_pfsense_config():
+    # Have it ignore the hostkey checks
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
+
+    if args.pfsense == 'false':
+        logger.error("pfSense IP not provided")
+        logger.info("Please provide the IP address / hostname of your pfSense")
+        hostname = input("Hostname / IP address of pfSense: ")
+    else:
+        hostname = args.pfsense
+
+    if args.username == 'false':
+        logger.error("Username not provided")
+        logger.info("Please provide the username to connect to pfSense as")
+        username = input("Username to SSH (SFTP) into pfSense as: ")
+    else:
+        username = args.username
+
+    with pysftp.Connection(hostname, username=username, private_key=args.pfsense_ssh_private_key, cnopts=cnopts) as sftp:
+        sftp.get('/conf/config.xml', "pfsense_config.xml")
+    return
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="This is the description for the main parser!"
     )
     parser.add_argument(
-        "required_arg",
-        help="Required. This is the description for the main requirement.",
+        "pfsense_ssh_private_key",
+        help="Required. Please provide the path to your private key to be used to SSH into pfSense.",
+    )
+    parser.add_argument(
+        "--pfsense",
+        type=str,
+        nargs=1,
+        default="false",
+        help="Optional. Provide the IP address / hostname of the pfSense that you wish to fetch the config of.",
+    )
+    parser.add_argument(
+        "--username",
+        type=str,
+        nargs=1,
+        default="false",
+        help="Optional. Provide the username to connect to pfSense as.",
     )
     parser.add_argument(
         "--debug",
@@ -21,5 +63,9 @@ if __name__ == "__main__":
 
     logger = log.get_logger(__file__, debug=args.debug)
 
-    logger.debug("This is the debug logger!")
-    logger.info(f"This is {__file__}")
+    print(pyfiglet.figlet_format("pfSense and PiHole Backup", font = "slant"))
+    logger.warning("Please be sure to enable SSH in your pfSense (System -> Advanced -> Admin Access")
+
+    get_pfsense_config()
+
+    logger.debug("Configs successfully retrieved.")
