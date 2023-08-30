12b22ba0fe8b627fea0bf6e48ebbb02c7ae1807a
perf3ct <jonfuller2012@gmail.com>
2023-08-30 16:52:21 -0700

    rename that var

--- backupinatorclient.py
+++ backupinatorclient.py
@@ -12,17 +12,17 @@ class BackupinatorClient(
     def __init__(self, debug=False):
         super().__init__(debug=debug)
 
-    def handle_backup(self, file_path: str, system: str):
+    def handle_backup(self, source_file_path: str, system: str):
         formatted_date = datetime.date.today().strftime("%m_%d_%y")
         new_filename = f"{formatted_date}_{system}.xml"
 
         if self.config["backup_to"] == "nextcloud":
             path = f"{self.config['nextcloud']['remote_backup_path']}/{new_filename}"
-            print("file_path")
-            print(file_path)
-            self.upload_file(file_path, path)
+            print("source_file_path")
+            print(source_file_path)
+            self.upload_file(source_file_path, path)
 
         if self.config["backup_to"] == "local":
             shutil.move(
-                file_path, f"{self.config['local']['backup_path']}/{new_filename}"
+                source_file_path, f"{self.config['local']['backup_path']}/{new_filename}"
             )
